eea2b5473d682fd9996bbb2843fb95a30a065f0e
linhung0319 <linhung0319@gmail.com>
2022-09-16 18:16:41 +0800

    feat: Add function create_folder in utils.py

---
+++ src/features/build_features.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+script_dir = os.path.dirname(__file__)
+sys.path.append(os.path.join(script_dir, "../../"))
+
+from src.features.utils import create_folder
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ src/features/utils.py
@@ -0,0 +1,5 @@
+import os
+
+def create_folder(fd):
+    if not os.path.exists(fd):
+        os.makedirs(fd)
